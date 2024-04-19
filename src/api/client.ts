86d8242a561d3e584580f18ba3eba34e0bd6252f
董睿琪 <21373095@buaa.edu.cn>
2024-04-19 16:55:42 +0800

    init

---
+++ src/api/client.ts
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import {Local, Session} from '/@/utils/storage'
+import { ElMessage } from 'element-plus'
+import { log } from 'console'
+
+function DELETE(url: string, options?: any) {
+  return axios({ method: 'DELETE', url, ...options })
+}
+
+function GET(url: string, options?: any) {
+  return axios({ method: 'GET', url, ...options })
+}
+
+function POST(url: string, options?: any) {
+  // if (
+  //   options?.data &&
+  //   options.headers?.['Content-Type'] !== 'multipart/form-data'
+  // )
+  //   options.data = qs.stringify(options.data, { arrayFormat: 'brackets' })
+  return axios({ method: 'POST', url, ...options })
+}
+
+function PUT(url: string, options?: any) {
+  // if (options?.data)
+  //   options.data = qs.stringify(options.data, { arrayFormat: 'brackets' })
+  return axios({ method: 'PUT', url, ...options })
+}
+
+async function withAuth(func: Function, url: String, options:any) {
+  try {
+    const headers = { Authorization: 'Bearer ' + Local.get('access') };
+    options.headers = headers;
+    console.log(options);
+    
+    const response = await func(url, options);
+    return response;
+  } catch (error:any) {
+    console.log(error.response.data.message)
+    if (error.response.request.status == 401) {
+        console.log("超时")
+        ElMessage({
+        type: 'error',
+        message: '连接超时，请重新登录'
+      })
+      Session.clear();
+      window.location.reload();
+    } else {
+      throw error
+    }
+  }
+}
+
+export default { DELETE, GET, POST, PUT, withAuth }
---
+++ src/views/booking/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>booking</p>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
