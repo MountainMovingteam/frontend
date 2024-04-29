fb58fc97b6bdb21c905273da88f42ba86e4c32d7
董睿琪 <21373095@buaa.edu.cn>
2024-04-30 00:12:52 +0800

    完成基本信息修改接口对接

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,6 +2,7 @@ import client from '../client'
 
 enum API{
     USER_INFO = 'api/base/getInfo', //个人信息
+    USER_EDIT_INFO = 'api/base/editInfo',
 }
 const baseURL = 'http://123.57.128.246:8000/'
 
@@ -17,5 +18,13 @@ export interface Request {
 export function reqInfo() {
     let url = baseURL + API.USER_INFO;
     const reponse = client.withAuth(client.GET, url, {});
+    return reponse
+}
+
+export function modifyBaseInfo(data: any){
+    let url = baseURL + API.USER_EDIT_INFO;
+    console.log(data);
+    
+    const reponse = client.withAuth(client.POST, url, {data:data});
     return reponse
 }
