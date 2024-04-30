178efd69db7332c57fc92d84759fd7899f0159fd
李子锐 <21373337@buaa.edu.cn>
2024-04-30 21:04:08 +0800

    修改baseUrl

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,30 +1,30 @@
-import client from '../client'
+import client from '../client';
 
-enum API{
-    USER_INFO = 'api/base/getInfo', //个人信息
-    USER_EDIT_INFO = 'api/base/editInfo',
+enum API {
+	USER_INFO = 'api/base/getInfo', //个人信息
+	USER_EDIT_INFO = 'api/base/editInfo',
 }
-const baseURL = 'http://123.57.128.246:8000/'
+const baseURL = 'http://47.93.19.22:8000';
 
 export interface Request {
-    avatar: string;
-    email: string;
-    password: string;
-    username: string;
-    role : string;
-    [key: string]: string;
+	avatar: string;
+	email: string;
+	password: string;
+	username: string;
+	role: string;
+	[key: string]: string;
 }
 
 export function reqInfo() {
-    let url = baseURL + API.USER_INFO;
-    const reponse = client.withAuth(client.GET, url, {});
-    return reponse
+	let url = baseURL + API.USER_INFO;
+	const reponse = client.withAuth(client.GET, url, {});
+	return reponse;
 }
 
-export function modifyBaseInfo(data: FormData){
-    let url = baseURL + API.USER_EDIT_INFO;
-    console.log(data);
-    
-    const reponse = client.withAuth(client.POST, url, {data:data});
-    return reponse
+export function modifyBaseInfo(data: FormData) {
+	let url = baseURL + API.USER_EDIT_INFO;
+	console.log(data);
+
+	const reponse = client.withAuth(client.POST, url, { data: data });
+	return reponse;
 }
