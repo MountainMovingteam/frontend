8c87b6e18d7febd6878b9b84dd219478f318469f
Li-ZiRui <139056301+Li-ZiRui@users.noreply.github.com>
2024-05-02 23:16:13 +0800

    [fix](讲解员模块)：完成对接查看、添加、删除接口 (#49)
    * [fix](讲解员模块)：调整现有接口结构
    
    * [fix](讲解员模块)：前后端对接
    
    * merge dev
    
    * fix url
    
    * 修改baseUrl
    
    * fix url
    
    * [fix](讲解员模块)：前后端对接
    
    ---------
    
    Co-authored-by: 李子锐 <21373337@buaa.edu.cn>

---
+++ src/types/commentator.d.ts
@@ -0,0 +1,15 @@
+export type Commentator = {
+	name: string;
+	num: string;
+	tag: number;
+	time_index: number;
+};
+
+export type ExportData = {
+	name: string;
+	num: string;
+	tag: string;
+	campus: string;
+	weekday: string;
+	session: string;
+};
