a9832815fdebb9b7572b595762b468396a42e194
董睿琪 <21373095@buaa.edu.cn>
2024-06-06 00:08:19 +0800

    消除无用数据

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -146,7 +146,6 @@ const getInfo = (start:number,end:number)  => {
 
 const load = () => {
 	if (info.infoArray.length >= info.maxInfos) {
-		getInfo(1,5);
 		return;
 	} else {
 		info.start = info.end + 1;
@@ -349,13 +348,15 @@ $homeNavLengh: 8;
 		.home-card-item {
 			width: 100%;
 			height: 100%;
-			border-radius: 4px;
+			@media only screen and (min-width: 768px) {padding: 20px;};
+			@media only screen and (min-width: 768px) {border-radius: 4px;};
+			
 			transition: all ease 0.3s;
-			padding: 20px;
 			overflow: hidden;
 			background: var(--el-color-white);
 			color: var(--el-text-color-primary);
-			border: 1px solid var(--next-border-color-light);
+			@media only screen and (min-width: 768px) {border: 1px solid var(--next-border-color-light);};
+			
 			&:hover {
 				box-shadow: 0 2px 12px var(--next-color-dark-hover);
 				transition: all ease 0.3s;
@@ -420,37 +421,6 @@ $homeNavLengh: 8;
 			height: 100%;
 			width: 100%;
 			overflow: hidden;
-			.home-monitor {
-				height: 100%;
-				.flex-warp-item {
-					width: 25%;
-					height: 111px;
-					display: flex;
-					.flex-warp-item-box {
-						margin: auto;
-						text-align: center;
-						color: var(--el-text-color-primary);
-						display: flex;
-						border-radius: 5px;
-						background: var(--next-bg-color);
-						cursor: pointer;
-						transition: all 0.3s ease;
-						&:hover {
-							background: var(--el-color-primary-light-9);
-							transition: all 0.3s ease;
-						}
-					}
-					@for $i from 0 through $homeNavLengh {
-						.home-animation#{$i} {
-							opacity: 0;
-							animation-name: error-num;
-							animation-duration: 0.5s;
-							animation-fill-mode: forwards;
-							animation-delay: calc($i/10) + s;
-						}
-					}
-				}
-			}
 		}
 	}
 }
