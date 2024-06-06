8e4391968ae7440f2bff8b3f36578d38c7a693e5
Xin Chen <leostar0328@outlook.com>
2024-06-06 16:36:23 +0800

    [feat](问答界面)：获取题目接口对接

--- src/views/answer/index.vue
+++ src/views/answer/index.vue
@@ -127,15 +127,16 @@
 </template>
 
 <script setup lang="ts">
-import {computed, onBeforeUnmount, onMounted, ref} from 'vue';
+
+import {computed, onMounted, ref} from 'vue';
 import {useRouter} from "vue-router";
 import {RefreshLeft} from "@element-plus/icons-vue";
+import {getAnswers, getQuestions} from "/@/api/answer";
 
 export interface Question {
   question_id: number;
   type: number;
   content: Content;
-  [property: string]: any;
 }
 
 export interface Content {
@@ -144,7 +145,6 @@ export interface Content {
   c_option: string;
   d_option: string;
   title: string;
-  [property: string]: any;
 }
 
 export interface Answer {
@@ -234,29 +234,11 @@ const handleCheckboxChange = (index: number, selectedOption: string) => {
 };
 
 const initQuestions = () => {
-  questions.value.splice(0, questions.value.length);
-  for (let i = 1; i <= 10; i++) {
-    let content = {
-      a_option: `${i}a******************** ******************** ******************************** **************** *********** *********************8`,
-      b_option: `${i}b问页面时：在哪里，去哪里，若是不合型网站。不合适就会引起用户操作不适（方向不明确）。 以下是索条、帮助按钮、`,
-      c_option: `${i}c哪里，去哪里，怎样去的问题。 一般导航会有「侧栏导航」和「顶部导航」2 种类型。选择合适的导航#选择哪里，去哪里，怎样去的问题。 一般导航会有「侧栏导航」和「顶部导航」2 种类型。选择合适的导航#选择 文字太多会溢出 只能这样调整`,
-      d_option: `${i}d侧栏导航#可导航栏固定在左侧，提高导航可见性，方便页面之间切换侧栏导航#可导航栏固定在左侧，提高导航可见性，方便页面之间切换`,
-      title: `这个题目的序号为${i}导航可以解决用户在访问页面时：在哪里，去哪里，怎样去的问题。 一般导航会有「侧栏导航」和「顶部导航」2 种类型。选择合适的导航#选择合适的导航可以让用户在产品的使用过程中非常流畅，相反若是不合适就会引起用户操作不适（方向不明确）。 以下是「侧栏导航」和 「顶部导航」的区别。
-      侧栏导航#可导航栏固定在左侧，提高导航可见性，方便页面之间切换； `,
-    };
-    let question = {
-      question_id: i,
-      type: i % 2,
-      content: content,
-    }
-    questions.value.push(question);
-  }
-  // ********** TODO ***************** 实际调用接口接受questionList的值
-  // const response = getQuestions(10);
-  // response.then(response =>{
-  //   state.questionList = response.data;
-  //   questions.value = response.data;
-  // });
+  const response = getQuestions(10);
+  response.then(response =>{
+    questions.value =  Object.values(response.data).find(Array.isArray);
+    console.log(questions.value)
+  });
 }
 
 const initAnswers = () => {
@@ -367,27 +349,18 @@ const switch2Main = () => {
 
 const submitAnswers = () => {
   let list: { question_id: number; student_answer: number[] }[] = [];
-  questions.value.forEach((item) => {
+  answers.value.forEach((item, index) => {
     let student_answer = generateNumberArray(item.a_option, item.b_option, item.c_option,item.d_option);
-    list.push({question_id: item.question_id, student_answer: student_answer});
+    list.push({question_id: questions.value[index].question_id, student_answer: student_answer});
   });
+  console.log(list);
   // ********** TODO ***************** 实际调用接口接受answerData的值
-  // const response = getAnswers(10, list);
-  // response.then(response=>{
-  //   state.answerData = response.data;
-  //   showResult.value = true;
-  // })
-  let objects = [];
-  for (let i = 1;i <= 10;i++) {
-    let object = {
-      question_id: i,
-      correct: i % 2 == 0,
-      correct_ans: i,
-    }
-    objects.push(object);
-  }
-  results.value = objects;
-  showResult.value = true;
+  const response = getAnswers(10, list);
+  response.then(response =>{
+    results.value = Object.values(response.data.list).find(Array.isArray);
+    console.log(results.value);
+    showResult.value = true;
+  })
 };
 </script>
 
