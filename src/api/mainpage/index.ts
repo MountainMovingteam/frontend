69edea9a54d2760a7fb3815b7f480636a2529742
MikeDrq <114203716+MikeDrq@users.noreply.github.com>
2024-06-11 21:39:27 +0800

    做题统计 (#125)
    Co-authored-by: 董睿琪 <“21373095@buaa.edu.cn”>

--- src/api/mainpage/index.ts
+++ src/api/mainpage/index.ts
@@ -3,7 +3,8 @@ import client from '../client';
 
 enum API {
 	INFO_LIST = '/api/base/push', //个人信息
-    PIC_LIST ='/api/base/pictures'
+    PIC_LIST ='/api/base/pictures',
+	HISTORY_LIST = '/api/question/query/history'
 	
 }
 const baseURL = 'http://47.93.19.22:8000';
@@ -25,6 +26,12 @@ export function getInfoList(data: any) {
 
 export function getPicList() {
 	let url = baseURL + API.PIC_LIST;
+	const reponse = client.withAuth(client.GET, url, {});
+	return reponse;
+}
+
+export function getQuestionHistory() {
+	let url = baseURL + API.HISTORY_LIST;
 	const reponse = client.withAuth(client.GET, url, {});
 	return reponse;
 }
